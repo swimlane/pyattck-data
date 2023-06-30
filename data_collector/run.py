42050fb288b7a3ef6768a591763dee15528b500e
Josh Rickard <jrickard@appomni.com>
2023-06-29 21:39:52 -0500

    Updating runs

---
+++ data_collector/run.py
@@ -0,0 +1,3 @@
+from data_collector import Collector
+
+Collector().collect()
--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from generate_data.generatenistdata import GenerateNISTData
 from generate_data.pyattckdata import PyattckData
 
-from data_collector import Collector
-
-Collector().collect()
-
 pydata = PyattckData()
 pydata.save()
 pydata.merge()
